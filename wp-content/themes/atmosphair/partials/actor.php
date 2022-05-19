3b06b1d6a53cad69b7cd93bb92051926e237baa5
natnatsch <nathan.schyns@student.hepl.be>
2022-05-19 21:34:36 +0200

    About mostly finished

--- wp-content/themes/atmosphair/partials/actor.php
+++ wp-content/themes/atmosphair/partials/actor.php
@@ -1,10 +1,12 @@
 <div class="actors__actor" itemscope itemtype="https://schema.org/Organization">
-    <h4 class="actor__title" itemprop="name"><?= get_field('nom', false, false); ?></h4>
-    <p class="actor__text">
-        <?= get_field('description', false, false); ?>
-    </p>
+    <div>
+        <h4 class="actor__title" itemprop="name"><?= get_field('nom', false, false); ?></h4>
+        <p class="actor__text">
+            <?= get_field('description', false, false); ?>
+        </p>
+        <a class="actor__link" href="<?= get_field('lien', false, false); ?>"><?= __('Visiter le site','atmosph\'air'); ?></a>
+    </div>
     <figure class="actor__fig">
         <?= get_the_post_thumbnail(null, 'medium', ['class' => 'actor__thumb']); ?>
     </figure>
-    <a class="actor__link" href="<?= get_field('lien', false, false); ?>"><?= __('Visiter le site','atmosph\'air'); ?></a>
 </div>
--- wp-content/themes/atmosphair/template-about.php
+++ wp-content/themes/atmosphair/template-about.php
@@ -1,11 +1,11 @@
 <?php /* Template Name: Template page À propos */ ?>
 <?php get_header(); ?>
-    <main class="main">
-        <h2 class="main__title"><?= __('Nous nous engageons à faire des trucs !','atmosph\'air'); ?></h2>
+    <main class="main__about">
         <video class="main__video" width="" height="" controls>
-            <source src="" type="">
+            <source src="<?= get_template_directory_uri(); ?>/src/images/bg_video.mp4" type="">
             <?= __('Votre navigateur ne supporte pas les balises vidéos.','atmosph\'air'); ?>
         </video>
+        <h2 class="main__title"><?= __('Nous nous engageons à ','atmosph\'air') . "<br>" . __('faire des trucs !','atmosph\'air'); ?></h2>
         <section class="main__actors">
             <h3 class="actors__title"><?= __('Les acteurs du projet','atmosph\'air'); ?></h3>
                 <?php if(($actors = atmos_get_actors(2))->have_posts()): while($actors->have_posts()): $actors->the_post();
@@ -15,20 +15,22 @@
                 <?php endif; ?>
         </section>
         <section class="main__morals">
-            <h3 class="morals__title"><?= __('Nos valeurs','atmosph\'air'); ?></h3>
+            <h3 class="morals__title hidden"><?= __('Nos valeurs','atmosph\'air'); ?></h3>
             <?php if(($morals = atmos_get_morals(3))->have_posts()): while($morals->have_posts()): $morals->the_post();
                 include (__DIR__ . '/partials/moral.php');
             endwhile; else:  ?>
-                <?= __('Nous n\'avons aucune valeurs, revenez plus tard.','atmosph\'air'); ?>
+                <?= __('Nous n\'avons aucunes valeurs, revenez plus tard.','atmosph\'air'); ?>
             <?php endif; ?>
         </section>
         <section class="main__timeline">
             <h3 class="timeline__title"><?= __('Petit historique du projet','atmosph\'air'); ?></h3>
-            <?php if(($dates = atmos_get_dates(10))->have_posts()): while($dates->have_posts()): $dates->the_post();
-                include (__DIR__ . '/partials/date.php');
-            endwhile; else:  ?>
-                <?= __('Il n\'y a pas d\'historique à afficher, revenez plus tard.','atmosph\'air'); ?>
-            <?php endif; ?>
+            <div class="timeline__container">
+                <?php if(($dates = atmos_get_dates(10))->have_posts()): while($dates->have_posts()): $dates->the_post();
+                    include (__DIR__ . '/partials/date.php');
+                endwhile; else:  ?>
+                    <?= __('Il n\'y a pas d\'historique à afficher, revenez plus tard.','atmosph\'air'); ?>
+                <?php endif; ?>
+            </div>
         </section>
     </main>
 <?php get_footer(); ?>
