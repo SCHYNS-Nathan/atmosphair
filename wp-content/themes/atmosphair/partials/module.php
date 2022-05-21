6788825dde189e7a82a7bed239274a768b487e56
nath3 <nathan.schyns@student.hepl.be>
2022-05-22 00:49:57 +0200

    Advanced on projects page

--- wp-content/themes/atmosphair/partials/module.php
+++ wp-content/themes/atmosphair/partials/module.php
@@ -1,11 +1,13 @@
 <li class="projects__item" itemscope itemtype="https://schema.org/Project">
     <article class="projects__project">
         <div>
-            <h4 class="project__title" itemprop="name"><?= get_the_title(); ?></h4>
+            <div class="project__name">
+                <h4 class="project__title" itemprop="name"><?= get_the_title(); ?></h4>
+                <img class="project__icon" src="<?= get_template_directory_uri() . "/src/images/" . get_field('nom', false, false) . ".svg"; ?>" alt="<?= __('Icône du projet','atmosph\'air'); ?>">
+            </div>
             <figure class="project__fig">
                 <?= get_the_post_thumbnail(null, 'medium', ['class' => 'actor__thumb']); ?>
             </figure>
-            <img class="project__icon" src="<?= get_template_directory_uri() . "/src/images/" . get_field('nom', false, false) . ".svg"; ?>" alt="<?= __('Icône du projet','atmosph\'air'); ?>">
             <p class="project__description" itemprop="description">
                 <?= get_field('description', false, false); ?>
             </p>
