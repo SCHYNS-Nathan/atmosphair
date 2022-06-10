a542a4303fc01fe72601dced3a98e23bb3f67b97
nath3 <nathan.schyns@student.hepl.be>
2022-06-10 23:37:47 +0200

    Added tests in single module

--- wp-content/themes/atmosphair/partials/publication.php
+++ wp-content/themes/atmosphair/partials/publication.php
@@ -1,4 +1,4 @@
-<li class="main__item fade-in" style="background-image: url(<?= get_the_post_thumbnail_url(null, 'medium', ['class' => 'actor__thumb']); ?>">
+<li class="main__item fade-in" style="background-image: url(<?= get_the_post_thumbnail_url(null, 'medium'); ?>">
     <div class="main__articleContainer">
         <article class="main__article" itemscope itemtype="https://schema.org/Article" aria-labelledby="publication-<?= get_field('titre', false, false); ?>">
             <a class="article__fulllink" href="<?= get_field('lien', false, false); ?>">
