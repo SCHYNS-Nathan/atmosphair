6942e5f2ee66e8bc1ae930b9f628d8785bc059e7
nath3 <nathan.schyns@student.hepl.be>
2022-06-11 16:51:46 +0200

    CSS ajustement

--- wp-content/themes/atmosphair/partials/date.php
+++ wp-content/themes/atmosphair/partials/date.php
@@ -1,9 +1,10 @@
 <div class="timeline__entry" itemscope itemtype="https://schema.org/Event" aria-labelledby="historic-<?= get_field('date'); ?>">
     <h4 class="timeline__date" itemprop="startData" aria-level="4" id="historic-<?= get_field('date'); ?>"><time><?= get_field('date'); ?></time></h4>
-    <p class="timeline__text" itemprop="description">
-        <?= get_field('explication', false, false); ?>
-    </p>
     <figure class="timeline__thumb">
         <?= get_the_post_thumbnail(); ?>
     </figure>
+    <p class="timeline__text" itemprop="description">
+        <?= get_field('explication', false, false); ?>
+    </p>
+
 </div>
